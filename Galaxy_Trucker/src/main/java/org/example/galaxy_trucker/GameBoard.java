0f707d5b77b46545f53ac17564011608e50d53b5
passo <riccardo.passolunghi@mail.polimi.it>
2025-03-07 18:21:36 +0100

    gameboard push

---
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/GameBoard.java
@@ -0,0 +1,17 @@
+package org.example.galaxy_trucker;
+
+import java.lang.*;
+
+
+public class GameBoard {
+    String prova1 = "prova1";
+
+    public void printMessage() {
+        System.out.println(prova1);
+    }
+
+    public static void main(String[] args) {
+        GameBoard board = new GameBoard();
+        board.printMessage();
+    }
+}
