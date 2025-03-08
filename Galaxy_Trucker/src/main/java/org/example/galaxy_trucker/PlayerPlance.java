5d4c923e6d3eeb5a4270765f8330bfd11557c55c
passo <riccardo.passolunghi@mail.polimi.it>
2025-03-08 14:11:35 +0100

    baseline PlayerPlance

--- Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/PlayerPlance.java
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/PlayerPlance.java
@@ -1,4 +1,12 @@
 package org.example.galaxy_trucker;
 
 public class PlayerPlance {
+
+    //attributes
+
+    public PlayerPlance() {};  // crea la matrice di riferimenti a tassello
+
+    public PlayerPlance getPlayerPlance(){
+        return this;
+    };
 }
