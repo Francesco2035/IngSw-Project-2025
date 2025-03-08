cf539fca1d8598f25f6f3fdfc4893581af7360f6
passo <riccardo.passolunghi@mail.polimi.it>
2025-03-08 14:24:48 +0100

    baseline PlayerPlance test

---
+++ Galaxy_Trucker/src/test/java/org/example/galaxy_trucker/PlayerPlanceTest.java
@@ -0,0 +1,12 @@
+package org.example.galaxy_trucker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerPlanceTest {
+
+    @Test
+    void getPlayerPlance() {
+    }
+}
