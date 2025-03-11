729a5093868fa80737217281eb13a6e6d9b4e350
pietro <pietro.poggi@mail.polimi.it>
2025-03-11 12:12:00 +0100

    Aggiunta StarPowder

---
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/StarPowder.java
@@ -0,0 +1,25 @@
+package org.example.galaxy_trucker;
+
+import java.util.ArrayList;
+
+public class StarPowder extends Card {
+    StarPowder(int level,GameBoard board){
+        super(level, 0 ,board);
+    }
+    public void CardEffect () {
+
+        GameBoard StarPowderBoard=this.getBoard();
+        ArrayList<Player> StarPowderPlayerList = StarPowderBoard.getPlayers();
+        PlayerPlance StarPowderCurrentPlanche;
+        int StarPowderOrder=StarPowderPlayerList.size();
+        int StarpowderMovement=0;
+        while(StarPowderOrder>=0){
+            StarPowderCurrentPlanche=StarPowderPlayerList.get(StarPowderOrder).getMyPlance();
+            //StarpowderMovement=-StarPowderCurrentPlanche.CountExposed();
+            //StarPowderPlayerList.get(StarPowderOrder).movePlayer(StarPowderMovement);
+            StarPowderOrder--;
+        }
+        return;
+    }
+}
+
