cde918ed4f750acef9a5a204fe99a17fe1d3f8fa
PaluFede <federico.paludetti@mail.polimi.it>
2025-03-15 00:43:23 +0100

    Miglioramento gestione del movimento dei player sul tabellone e gestione della leadboard

--- Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/GameBoard.java
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/GameBoard.java
@@ -18,7 +18,7 @@ public class GameBoard {
     private Hourglass hourglass;
     private int GameLv;
     private int PlayersOnBoard;
-    private int nPlayers;
+
 
 
     public GameBoard(TileSets list, int lv) {
@@ -27,22 +27,23 @@ public class GameBoard {
         tileSets = list;
         startPos = new int[4];
         PlayersOnBoard = 0;
-        nPlayers =0;
+
 
         if(lv == 2) {
             nPositions = 24;
             startPos[0] = 6;
             startPos[1] = 3;
-            startPos[2] = 1;
-            startPos[3] = 0;
+
         }
         else {
             nPositions = 18;
             startPos[0] = 4;
             startPos[1] = 2;
-            startPos[2] = 1;
-            startPos[3] = 0;
         }
+
+        startPos[2] = 1;
+        startPos[3] = 0;
+
         positions = new Player[nPositions];
         hourglass = new Hourglass(GameLv);
     }
@@ -52,7 +53,7 @@ public class GameBoard {
         int NewPlayerPosition = 0;
         Pair<Player, Integer> NewPair = new Pair<>(NewPlayer, NewPlayerPosition);
         this.players.add(NewPair);
-        nPlayers++;
+
     }
 
 
@@ -60,16 +61,27 @@ public class GameBoard {
         //the position players[0] corresponds to the starting position of the 1st player
         //the leader (1st player) will be in the first position of the arraylist
 
-        Pair<Player, Integer> cur = players.stream().filter(p -> ID.equals( p.getKey().GetID()) ).findFirst().orElseThrow();
+        Pair<Player, Integer> cur = players.stream()
+                .filter(p -> ID
+                .equals( p.getKey().GetID()) )
+                .findFirst().orElseThrow();
 
         SetNewPosition(cur, startPos[PlayersOnBoard]);
+
         PlayersOnBoard++;
     }
 
 
     public void movePlayer(String ID, int nSteps) throws IllegalArgumentException{
-        Pair<Player, Integer> cur = players.stream().filter(p -> ID.equals( p.getKey().GetID()) ).findFirst().orElseThrow();
+        Pair<Player, Integer> cur = players
+                .stream()
+                .filter(p -> ID.equals( p.getKey().GetID() ) )
+                .findFirst()
+                .orElseThrow();
+
         int NewPos = cur.getValue();
+
+        //AGGIUNGERE GESTIONE DOPPIAGGIO
 
         if(nSteps == 0) throw new IllegalArgumentException("Number of steps cannot be 0: must move forward or backwards");
         else if(nSteps > 0)
@@ -78,34 +90,42 @@ public class GameBoard {
                 if(positions[NewPos] == null) nSteps--;
             }
         else while(nSteps < 0){
-        NewPos--;
-        if(positions[NewPos] == null) nSteps++;
+            NewPos--;
+            if(positions[NewPos] == null) nSteps++;
         }
 
         SetNewPosition(cur, NewPos);
     }
 
 
-    private void SetNewPosition(Pair<Player, Integer> cur, int newPosition){
 
+    private void SetNewPosition(Pair<Player, Integer> cur, int newPosition){
         int CurIndex = players.indexOf(cur);
         Pair<Player, Integer> NewPair = new Pair<>(cur.getKey(), newPosition);
 
         positions[cur.getValue()] = null;
-        positions[newPosition] = NewPair.getKey();
+        positions[newPosition % nPositions] = NewPair.getKey();
 
         players.remove(CurIndex);
         players.add(CurIndex, NewPair);
 
         players.sort(Comparator.comparing(Pair::getValue));
+
+        //revers the order of the arraylist to have the leader in the 1st position
+        ArrayList<Pair<Player, Integer>> OrderedPlayers = new ArrayList<>();
+        for(int i= players.size()-1; i>=0; i--){
+            OrderedPlayers.add(players.get(i));
+        }
+        players = OrderedPlayers;
+
     }
 
 
     public ArrayList<Player> getPlayers(){
         ArrayList<Player> PlayersCopy = new ArrayList<>();
 
-        for(int i=0; i<nPlayers; i++) {
-            PlayersCopy.add(players.get(i).getKey());
+        for (Pair<Player, Integer> player : players) {
+            PlayersCopy.add(player.getKey());
         }
 
         return PlayersCopy;
@@ -113,5 +133,6 @@ public class GameBoard {
 
     public int getLevel(){return GameLv;}
     public Player[] getPositions(){return this.positions;}
+
 
 }
--- Galaxy_Trucker/src/test/java/org/example/galaxy_trucker/GameBoardTest.java
+++ Galaxy_Trucker/src/test/java/org/example/galaxy_trucker/GameBoardTest.java
@@ -14,7 +14,7 @@ class GameBoardTest {
 
         assertEquals(1, gb.getPlayers().size());
         for(Player p : gb.getPositions()){
-            assertEquals(null, p);      //tests that player array is correctly initialize with null
+            assertNull(p);      //tests that player array is correctly initialize with null
         }
     }
 
@@ -31,6 +31,7 @@ class GameBoardTest {
 
         assertEquals("Player1", gb.getPositions()[6].GetID());
         assertEquals("Player2", gb.getPositions()[3].GetID());
+
         assertNull(gb.getPositions()[4]);
         assertNull(gb.getPositions()[0]);
 
@@ -43,12 +44,33 @@ class GameBoardTest {
         GameBoard gb = new GameBoard(tileSets, 2);
 
         gb.addPlayer("Player1");
+        gb.addPlayer("Player2");
+        gb.addPlayer("Player3");
 
         gb.SetStartingPosition("Player1");
+        gb.SetStartingPosition("Player2");
+        gb.SetStartingPosition("Player3");
+
 
         assertEquals("Player1", gb.getPositions()[6].GetID());
-        gb.movePlayer("Player1", 4);
-        assertEquals("Player1", gb.getPositions()[10].GetID());
-        assertNull(gb.getPositions()[6]);
+        assertEquals("Player2", gb.getPositions()[3].GetID());
+        assertEquals("Player3", gb.getPositions()[1].GetID());
+
+
+        gb.movePlayer("Player3", 2);
+        assertEquals("Player2", gb.getPositions()[3].GetID());
+        assertEquals("Player3", gb.getPositions()[4].GetID());
+        assertNull(gb.getPositions()[1]);
+
+        gb.movePlayer("Player3", -2);
+        assertEquals("Player3", gb.getPositions()[1].GetID());
+        assertNull(gb.getPositions()[4]);
+
+        gb.movePlayer("Player3", 5);
+
+        assertEquals("Player1", gb.getPositions()[6].GetID());
+        assertEquals("Player3", gb.getPositions()[8].GetID());
+        assertNull(gb.getPositions()[4]);
+
     }
 }
