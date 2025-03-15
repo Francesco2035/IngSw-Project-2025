62dca3dfee6006241db6fc331d172e1d45231d49
passo <riccardo.passolunghi@mail.polimi.it>
2025-03-15 16:00:35 +0100

    getSbility for hotWaterHeater

--- Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/hotWaterHeater.java
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/hotWaterHeater.java
@@ -10,6 +10,13 @@ public class hotWaterHeater extends Component{
 
 
     @Override
+    public int getAbility() {
+        if (isDouble) {return 2;}
+        else {return 1;}
+    }
+
+
+    @Override
     public void initType(){
         if (type.equals("single")) isDouble = false;
         else if (type.equals("double")) isDouble = true;
