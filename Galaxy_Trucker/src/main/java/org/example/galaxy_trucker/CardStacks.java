ccc94b2b5c302530ba9ee4e095dc3ef6f3feb32d
pietro <pietro.poggi@mail.polimi.it>
2025-03-16 17:02:34 +0100

    Card changes

--- Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/CardStacks.java
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/CardStacks.java
@@ -1,7 +1,25 @@
 package org.example.galaxy_trucker;
 
+import java.util.ArrayList;
+
 public class CardStacks {
 
+    public ArrayList<Card> VisibleCards1;
+    public ArrayList<Card> VisibleCards2;
+    public ArrayList<Card> VisibleCards3;
+    private ArrayList<Card> HiddenCards;
+    private ArrayList<Card> FullAdventure;
+    private ArrayList<Card> Level1Deck;
+    private ArrayList<Card> Level2Deck;
+
+
+public CardStacks() {
+    //dipende da come vien creato dal json
+}
+
+private void makedecks(int n){
+
+}
 
 
 }
