7e893c46fd9bb17710855d57f8c79a77165bd48e
Francesco <francesco.rausa@mail.polimi.it>
2025-03-17 18:48:04 +0100

    sono obbligato a farlo

--- Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/CustomExceptions/BufferOverflowException.java
+++
@@ -1,7 +0,0 @@
-package org.example.galaxy_trucker.CustomExceptions;
-
-public class BufferOverflowException extends RuntimeException {
-    public BufferOverflowException(String message) {
-        super(message);
-    }
-}
