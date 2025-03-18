e337b1df460cddcf3c812ad85bcb24d4b19adfd5
passo <riccardo.passolunghi@mail.polimi.it>
2025-03-18 11:23:20 +0100

    gagen fix + tests

--- Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/GAGen.java
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/GAGen.java
@@ -1,36 +1,32 @@
 package org.example.galaxy_trucker;
 
-import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class GAGen {
 
     private ObjectMapper mapper = new ObjectMapper();
     private File TilesJSON = new File("src/main/resources/org/example/galaxy_trucker/Tiles.JSON");
-    private ArrayList<Tile> deck;
-
+    private File CardsJSON = new File("src/main/resources/org/example/galaxy_trucker/Cards.JSON");  //add file json
+    private ArrayList<Tile> tilesDeck;
+    private ArrayList<Tile> cardsDeck;
 
 
     //parsa json per connectors e component e chiama il builder di tile ripetuto fino a EOF
     public GAGen() throws IOException {
-        this.deck = mapper.readValue(TilesJSON, new TypeReference<ArrayList<Tile>>() {});
+        this.tilesDeck = mapper.readValue(TilesJSON, new TypeReference<ArrayList<Tile>>() {});
+        this.cardsDeck = mapper.readValue(CardsJSON, new TypeReference<ArrayList<Tile>>() {});
     }
 
 
-    public ArrayList<Tile> getDeck() {
-        return deck;
+    //getters for tilesdeck and cardsdeck
+    public ArrayList<Tile> getTilesDeck() {
+        return tilesDeck;
     }
-
-    public void setDeck(ArrayList<Tile> deck) {this.deck = deck;}
-
+    public ArrayList<Tile> getCardsDeck() {return cardsDeck;}
 
 }
