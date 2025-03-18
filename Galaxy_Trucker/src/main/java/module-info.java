6752341b44bbdc8760f9ce67d8144f2ff0d8b754
Francesco <francesco.rausa@mail.polimi.it>
2025-03-18 23:08:10 +0100

    fix test

--- Galaxy_Trucker/src/main/java/module-info.java
+++ Galaxy_Trucker/src/main/java/module-info.java
@@ -8,7 +8,7 @@ module org.example.galaxy_trucker {
     requires org.jetbrains.annotations;
 
     opens org.example.galaxy_trucker to javafx.fxml, com.fasterxml.jackson.databind;
-    exports org.example.galaxy_trucker;
+    //exports org.example.galaxy_trucker.;
     exports org.example.galaxy_trucker.Model.Tiles;
     opens org.example.galaxy_trucker.Model.Tiles to com.fasterxml.jackson.databind, javafx.fxml;
     exports org.example.galaxy_trucker.Model.Cards;
