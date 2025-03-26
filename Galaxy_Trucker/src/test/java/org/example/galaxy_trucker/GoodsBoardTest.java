35556d81255b1775da0e22147a559f849a498e33
passo <riccardo.passolunghi@mail.polimi.it>
2025-03-26 11:46:57 +0100

    merge with francesco part2

--- Galaxy_Trucker/src/test/java/org/example/galaxy_trucker/GoodsBoardTest.java
+++ Galaxy_Trucker/src/test/java/org/example/galaxy_trucker/GoodsBoardTest.java
@@ -2,6 +2,8 @@ package org.example.galaxy_trucker;
 
 import org.example.galaxy_trucker.Model.Boards.Goods;
 import org.example.galaxy_trucker.Model.Boards.PlayerBoard;
+import org.example.galaxy_trucker.Model.Goods.BLUE;
+import org.example.galaxy_trucker.Model.Goods.GREEN;
 import org.example.galaxy_trucker.Model.Tiles.specialStorageCompartment;
 import org.example.galaxy_trucker.Model.Tiles.storageCompartment;
 import org.junit.jupiter.api.Assertions;
@@ -26,8 +28,8 @@ class GoodsBoardTest {
         storageCompartment storage1 = new storageCompartment();
         specialStorageCompartment storage2 = new specialStorageCompartment();
         storage1.setMaxNumGoods(3);
-        storage1.setAbility(Goods.BLUE, true);
-        storage1.setAbility(Goods.GREEN, true);
+        storage1.setAbility(new BLUE(), true);
+        storage1.setAbility(new GREEN(), true);
         Assertions.assertEquals(Goods.GREEN, storage1.getGoods().get(0));
 
         PlayerBoard testboard= new PlayerBoard(2);
