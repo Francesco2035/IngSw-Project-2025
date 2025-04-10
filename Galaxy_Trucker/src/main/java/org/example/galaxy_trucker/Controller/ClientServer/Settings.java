19a97061b9de957ffa8359ab778954098d2fd343
PaluFede <federico.paludetti@mail.polimi.it>
2025-04-10 11:45:43 +0200

    set servername

--- Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/Controller/ClientServer/Settings.java
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/Controller/ClientServer/Settings.java
@@ -2,5 +2,5 @@ package org.example.galaxy_trucker.Controller.ClientServer;
 
 public class Settings {
     public static int PORT = 6969;
-    public static String SERVER_NAME = "192.168.164.72";
+    public static String SERVER_NAME = "192.168.1.3";
 }
