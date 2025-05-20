815523e70f4b25f5ac0b3fac43938c4a0c37726d
passo <riccardo.passolunghi@mail.polimi.it>
2025-05-20 13:35:29 +0200

    fix test login controller

--- Galaxy_Trucker/src/test/java/org/example/galaxy_trucker/Controller/LoginControllerTest.java
+++ Galaxy_Trucker/src/test/java/org/example/galaxy_trucker/Controller/LoginControllerTest.java
@@ -2,8 +2,8 @@ package org.example.galaxy_trucker.Controller;
 
 import org.example.galaxy_trucker.Commands.Command;
 import org.example.galaxy_trucker.Commands.LoginCommand;
-import org.example.galaxy_trucker.Controller.ClientServer.Client;
-import org.example.galaxy_trucker.Controller.ClientServer.ServersHandler;
+import org.example.galaxy_trucker.ClientServer.Client;
+import org.example.galaxy_trucker.ClientServer.ServersHandler;
 import org.example.galaxy_trucker.Model.Game;
 import org.example.galaxy_trucker.Model.Player;
 import org.junit.jupiter.api.Test;
@@ -33,7 +33,7 @@ class LoginControllerTest {
         Thread clientThread = new Thread(() -> {
             try {
                 Client client = new Client();
-                client.run(args);
+                client.run();
             } catch (Exception e) {
                 e.printStackTrace();
                 fail("Errore nell'avvio del client: " + e.getMessage());
