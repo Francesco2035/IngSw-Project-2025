ee37b035d68b91be2ee11011e1b16dff319d266c
Francesco <francesco.rausa@mail.polimi.it>
2025-05-20 15:55:05 +0200

    aggiunta listener per fase di concorrenza (ricordarsi i usare il metodo XD)

---
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/Controller/Messages/ConcurrentCardListener.java
@@ -0,0 +1,9 @@
+package org.example.galaxy_trucker.Controller.Messages;
+
+import org.example.galaxy_trucker.Controller.Listeners.GameLobbyListener;
+
+public interface ConcurrentCardListener {
+
+    public void onConcurrentCard(boolean phase);
+
+}
