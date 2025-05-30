daeddab2bbb71e2115e9a73a20ff155faa62573a
Francesco <francesco.rausa@mail.polimi.it>
2025-05-30 18:14:37 +0200

    aggiunta di descrizioni comandi

--- Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/View/TUI/CommandCompleter.java
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/View/TUI/CommandCompleter.java
@@ -21,9 +21,17 @@ public class CommandCompleter implements DynamicCompleter {
         description = new HashMap<>();
         description.put("login", "Login");
         description.put("lobby", "Lobby");
-        description.put("kill", "x y");
+        description.put("create", "create new game");
+        description.put("kill", "x y x y ... : kill 1 human in the selected tile x,y");
         description.put("inserttile", "x y rotation");
         description.put("discard", "");
+        description.put("picktile", "no arg-> covered : index -> uncovered");
+        description.put("addcrew", "x y : adds 2 humans in the selected tile");
+        description.put("addbrownalien", "x y");
+        description.put("addpurplealien", "x y");
+        description.put("getreward", "i_1 x y i_2 : put the good in position i_1 \n" +
+                                 "in the selected tile and in the i_2 position");
+        description.put("finishcargo", ":finish handlecargo phase");
     }
     @Override
     public void complete(LineReader reader, ParsedLine line, List<Candidate> candidates) {
