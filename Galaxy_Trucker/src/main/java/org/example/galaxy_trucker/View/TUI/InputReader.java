99230863431b74889bf5dcc4835937cf4546e39f
Francesco <francesco.rausa@mail.polimi.it>
2025-06-06 12:41:33 +0200

    .

--- Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/View/TUI/InputReader.java
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/View/TUI/InputReader.java
@@ -139,18 +139,18 @@ public class InputReader implements Runnable {
 
     public synchronized void renderScreen(StringBuilder content) {
 
-//        try{
-//            if(System.getProperty("os.name").contains("Windows")){
-//                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-//            }
-//            else
-//                Runtime.getRuntime().exec("clear");
-//        }
-//        catch (IOException | InterruptedException _){
-//            ;
-//        }
-//        System.out.print("\033[H\033[2J");
-//        System.out.flush();
+        try{
+            if(System.getProperty("os.name").contains("Windows")){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            }
+            else
+                Runtime.getRuntime().exec("clear");
+        }
+        catch (IOException | InterruptedException _){
+            ;
+        }
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
         //TODO: capire quale sistema operativo è e fare clean di conseguenza
 
         String partialInput = Lreader.getBuffer().toString();
