9cf1e9f734b6a3d547d70d3e5c3dd6be7a8a2a0a
passo <riccardo.passolunghi@mail.polimi.it>
2025-06-14 11:54:01 +0200

    mini PlayerBoard fix

--- Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/Model/Boards/PlayerBoard.java
+++ Galaxy_Trucker/src/main/java/org/example/galaxy_trucker/Model/Boards/PlayerBoard.java
@@ -1137,10 +1137,10 @@ public class PlayerBoard {
             if (purpleAlien){
                 crew++;
             }
-            if (purpleAlien){
+            if (brownAlien){
                 crew++;
             }
-            int engine = 0;
+//            int engine = 0;
             PBInfoEvent event = new PBInfoEvent(this.damage, this.credits, this.exposedConnectors, this.shield,crew, this.EnginePower, this.PlasmaDrillsPower, this.Energy, this.purpleAlien, this.brownAlien, this.totalValue);
             listener.PBInfoChanged(event);
         }
